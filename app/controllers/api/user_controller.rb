1e347d37f9cd1eb121464fd624d6424d5fd4a53d
Bisaso Ronald <bronnie313@gmail.com>
2023-09-29 19:09:06 +0300

    add actions for the user api

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -1,2 +1,23 @@
 class Api::UserController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def create
+    respond_to do |format|
+      @user = User.new(user_params)
+
+      if @user.save
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
