b24bd84819426ed83af1cd2242a2470676e48564
Ismail Ali <ismailchangezi801@gmail.com>
2023-09-29 20:19:27 +0500

    Create Model and Controllers: App is working fine

--- app/controllers/api/helicopter_controller.rb
+++ app/controllers/api/helicopter_controller.rb
@@ -1,21 +1,22 @@
 class Api::HelicopterController < ApplicationController
-    def index 
-        @helicopters = Helicopter.all
-        render json: @helicopters
+  def index
+    @helicopters = Helicopter.all
+    render json: @helicopters
+  end
+
+  def create
+    @helicopters = Helicopter.new(helicopter_params)
+
+    if @helicopter.save
+      render json: @helicopter, status: 200
+    else
+      render json: {
+        error: 'Error creating location...'
+      }
     end
-    def create
-        @helicopters = Helicopter.new(helicopter_params)
-    
-        if @helicopter.save
-          render json: @helicopter, status: 200
-        else
-          render json: {
-            error: 'Error creating location...'
-          }
-        end
-      end
-    
-      def location_params
-        params.require(:location).permit(:name, :description, :contact, :price, :carriage_capacity, :model)
-      end
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :description, :contact, :price, :carriage_capacity, :model)
+  end
 end
