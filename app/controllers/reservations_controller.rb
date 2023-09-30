b6446c51e72320df81c627aa4e0783b10ac02c05
Reggeditt <tkodjosarso@gmail.com>
2023-09-30 11:32:08 +0000

    add api routes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,9 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.all
+    render json: @reservations
+  end
+
   def create
     @reservation = Reservation.create(reservation_params)
     if @reservation.valid?
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:index, :create]
     resources :helicopter, only: [:index, :create]
+    post "/api/reservation", to: "reservations#create"
+    get "/api/reservations", to: "reservations#index"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
