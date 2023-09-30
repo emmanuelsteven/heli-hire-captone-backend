e0195680d2b11784a7e47127f2342cd4a82733c9
Reggeditt <tkodjosarso@gmail.com>
2023-09-30 11:24:30 +0000

    add reservation controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,16 @@
+class ReservationsController < ApplicationController
+    def create
+        @reservation = Reservation.create(reservation_params)
+        if @reservation.valid?
+            render json: @reservation, status: :created
+        else
+            render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def reservation_params
+        params.permit(:user_id, :helicopter_id, :city, :reserved_date_start, :reserved_date_end)
+    end
+end
