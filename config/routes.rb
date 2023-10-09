e49422591d54f8d9b6ce09107673cb45f4aaaf01
Reggeditt <tkodjosarso@gmail.com>
2023-10-09 09:01:02 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users, only: [:index, :create]
     resources :helicopter, only: [:index, :create, :destroy]
-    post "/api/reservation", to: "reservations#create"
-    get "/api/reservations", to: "reservations#index"
-    delete "/api/reservations/:id", to: "reservations#destroy"
+    resources :reservations, only: [:index, :create, :destroy]
 
   end
 end
