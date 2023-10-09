980f792466ff72bff59563f593d913143e73a7d1
Reggeditt <tkodjosarso@gmail.com>
2023-10-09 08:24:41 +0000

    update reservations model and table

--- db/migrate/20231009064239_update_reservation_table.rb
+++ db/migrate/20231009064239_update_reservation_table.rb
@@ -1,4 +1,8 @@
 class UpdateReservationTable < ActiveRecord::Migration[7.0]
   def change
+    # rename :reserved_date_start to :date on reservations table
+    rename_column :reservations, :reserved_date_start, :date
+    # remove :reserved_date_end from reservations table
+    remove_column :reservations, :reserved_date_end
   end
 end
