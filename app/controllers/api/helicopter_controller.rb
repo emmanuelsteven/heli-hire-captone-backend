ea7c79e26e1f3b5478c46419df905d27c4dad9fe
emmanuelsteven <write2stevemmy@gmail.com>
2023-10-12 18:05:38 +0100

    update helicopter controller

--- app/controllers/api/helicopter_controller.rb
+++ app/controllers/api/helicopter_controller.rb
@@ -20,16 +20,25 @@ class Api::HelicopterController < ApplicationController
       }
     end
   end
-
+  
   def destroy
-    @helicopter = Helicopter.find(params[:id])
-    @reservation = Reservation.where(helicopter_id: @helicopter.id)
-    @reservation.destroy_all
-    @helicopter.destroy
-    render json: { message: 'helicopter successfully deleted...' }
-    head :no_content
+    begin
+      @helicopter = Helicopter.find(params[:id])
+      
+      @reservation = Reservation.where(helicopter_id: @helicopter.id)
+      
+      if @reservation.present?
+        @reservation.destroy_all
+      end
+      @helicopter.destroy
+      render json: { message: 'Helicopter successfully deleted...' }
+      head :no_content
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: 'Helicopter not found' }, status: :not_found
+    rescue StandardError => e
+      render json: { error: 'An error occurred while deleting the helicopter' }, status: :internal_server_error
+    end
   end
-
   private
 
   def helicopter_params
