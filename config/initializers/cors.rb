43a034f691065303d9006f2d34182987e2ffa296
emmanuelsteven <write2stevemmy@gmail.com>
2023-10-24 17:31:24 +0100

    key update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "https://heli-hire-captsone-frontend.vercel.app/"
+      origins "*"
       resource "*",
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
